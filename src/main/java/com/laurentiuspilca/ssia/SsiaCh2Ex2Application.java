499b3842cad65fcc6ac1452a037e90d1d1fdb98c
serghalak <serghalak@gmail.com>
2020-12-22 14:10:04 +0200

    first commit

---
+++ src/main/java/com/laurentiuspilca/ssia/SsiaCh2Ex2Application.java
@@ -0,0 +1,13 @@
+package com.laurentiuspilca.ssia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiaCh2Ex2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsiaCh2Ex2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/laurentiuspilca/ssia/SsiaCh2Ex2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laurentiuspilca.ssia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiaCh2Ex2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
