36e2fd55370c52ce18e38e8f18053fe3cd461777
serghalak <serghalak@gmail.com>
2020-12-22 21:11:20 +0200

    add implements of authenticationprovider

--- src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
+++ src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
@@ -2,9 +2,11 @@ package com.laurentiuspilca.ssia.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,6 +14,35 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//
+//        auth.inMemoryAuthentication()
+//                    .withUser("john")
+//                    .password("12345")
+//                    .authorities("read")
+//                .and()
+//                    .passwordEncoder(NoOpPasswordEncoder.getInstance());
+//    }
+
+
+    //    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//
+//        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+//
+//        UserDetailsService userDetailsService= inMemoryUserDetailsManager;
+//
+//        UserDetails userDetails=
+//                User.withUsername("john")
+//                        .password("12345")
+//                        .authorities("read")
+//                        .build();
+//        inMemoryUserDetailsManager.createUser(userDetails);
+//
+//        auth.userDetailsService(userDetailsService)
+//                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+//    }
 
     @Override
     @Bean
